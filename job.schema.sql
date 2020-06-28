b1db384e16d9a0f79aad7ab9821a105cd6c12597
Stefan Stefanov <stefanov.sm@abv.bg>
2020-06-28 08:25:26 +0300

    Update job.schema.sql

--- job.schema.sql
+++ job.schema.sql
@@ -42,12 +42,12 @@ insert into jobs.job_schedule (schedule, job_name, job_definition) values
 (
  '{"day": [], "dow": [], "mon": [], "repeat": {"from": "00:00", "till": "23:59", "every": "PT3M"}}',
  'Repeat this every three minutes', 
- '{"query": "select 1 as one", "message": "The one that repeats every three minutes"}'
+ '{"query": "select 1 as one", "message": "Repeats every three minutes"}'
 ), 
 (
  '{"day": [28], "dow": [], "mon": [6, 7, 8], "time": ["00:30"]}',
  'Run only once in June, July and August',
- '{"message": "The one that runs once per month", "process": "run-me.exe"}'
+ '{"message": "Runs once per month", "process": "run-me.exe"}'
 );
 
 create table if not exists jobs.log 
