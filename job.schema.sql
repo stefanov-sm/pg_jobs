746f4be727143448893e9c9aee7e606e7e1ff102
Stefan Stefanov <stefanov.sm@abv.bg>
2020-06-29 18:18:50 +0300

    Update job.schema.sql

--- job.schema.sql
+++ job.schema.sql
@@ -36,23 +36,3 @@ $$
  and (coalesce(jsonb_array_length(schedule->'day'), 0) = 0 or extract("DAY"    from now()) in (select i::integer from jsonb_array_elements_text(schedule->'day') i))
  and (coalesce(jsonb_array_length(schedule->'mon'), 0) = 0 or extract("MONTH"  from now()) in (select i::integer from jsonb_array_elements_text(schedule->'mon') i));
 $$;
-
--- The example
-insert into jobs.job_schedule (schedule, job_name, job_definition) values 
-(
- '{"day": [], "dow": [], "mon": [], "repeat": {"from": "00:00", "till": "23:59", "every": "PT3M"}}',
- 'Repeat this every three minutes', 
- '{"query": "select 1 as one", "message": "Repeats every three minutes"}'
-), 
-(
- '{"day": [28], "dow": [], "mon": [6, 7, 8], "time": ["00:30"]}',
- 'Run only once in June, July and August',
- '{"message": "Runs once per month", "process": "run-me.exe"}'
-);
-
-create table if not exists jobs.log 
-(
- message text,
- time_created timestamp default now()
-);
-
